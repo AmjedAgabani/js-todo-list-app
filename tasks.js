9818d1bc447e43ab33239cf7b3c394731381188b
Amjed Agabani <amjedagabani@gmail.com>
2025-09-11 14:44:20 +0100

    Create tasks.js

---
+++ tasks.js
@@ -0,0 +1,20 @@
+export let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+export function saveTasks() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+export function addTask(text) {
+  tasks.push({ text, completed: false });
+  saveTasks();
+}
+
+export function deleteTask(index) {
+  tasks.splice(index, 1);
+  saveTasks();
+}
+
+export function toggleTaskCompletion(index) {
+  tasks[index].completed = !tasks[index].completed;
+  saveTasks();
+}
